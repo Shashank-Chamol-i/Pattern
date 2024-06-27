30fb610de061351621f9cb8a392938359bf02a74
Shashank-Chamol-i <shashankchamoli069@gmail.com>
2024-06-27 17:59:45 +0530

    Add files via upload

---
+++ Pattern_4.java
@@ -0,0 +1,16 @@
+
+public class Pattern_4 {
+
+	public static void main(String[] args) {
+		for(int i = 5;i>0;--i)
+		{
+			for(int j = 5;j>0;j--)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ Pattern_5.java
@@ -0,0 +1,16 @@
+
+public class Pattern_5 {
+
+	public static void main(String[] args) {
+		for(int i = 5;i>0;i--)
+		{
+			for(int j = 5;j>0;j--)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
